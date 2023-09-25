a102c4fb8c93a2d99240da55593c82e96bf8dae7
FranciscoJaguaco <56598601+FranciscoJaguaco@users.noreply.github.com>
2023-09-25 15:45:59 -0500

    Deber1
    Deber 1

---
+++ problemas1.py
@@ -0,0 +1,95 @@
+''' CADA EJERCICIO DEBE EJECUTARSE INDIVIDUALMENTE'''
+
+'''EJERCICIO 1 '''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+respuesta=a+b*c/d
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 2'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+respuesta=(a/b)*(c+d)
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 3'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+respuesta=a-b*c**d
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 4'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+e=int(input('Ingrese el quinto valor(e):'))
+respuesta=(a+b)*c-(d/e)
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 5'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+respuesta=(a*b)%c
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 6'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+respuesta=a**b**c
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 7'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+e=int(input('Ingrese el quinto valor(e):'))
+respuesta=[(((a+b)-c)*d)/e]
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+
+''' EJERCICIO 8'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+e=int(input('Ingrese el quinto valor(e):'))
+respuesta=(a*(b+c)/(d-e))/2
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+''' EJERCICIO 9'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+respuesta=((a+b)**c)
+print('La respuesta es de la operacion:')
+print(respuesta)
+
+
+''' EJERCICIO 10'''
+a=int(input('Ingrese el primer valor(a):'))
+b=int(input('Ingrese el segundo valor(b):'))
+c=int(input('Ingrese el tercer valor(c):'))
+d=int(input('Ingrese el cuarto valor(d):'))
+e=int(input('Ingrese el quinto valor(e):'))
+respuesta=(((a-b)*(c+d))/e)
+print('La respuesta es de la operacion:')
+print(respuesta)
+
